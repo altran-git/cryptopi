c0cf6f4f736225c37409cbc37d94145b2715b224
Andy <trnandy@gmail.com>
2017-06-07 01:41:42 -0700

    Fix

--- cryptopi.py
+++ cryptopi.py
@@ -25,7 +25,7 @@ class Worker(threading.Thread):
         while True:
             url = 'https://min-api.cryptocompare.com/data/histohour?fsym={}&tsym={}&limit=24'.format(self.fsym.upper(),self.tsym.upper())
             self.data_24hr = json.loads(urlopen(url).read().decode('utf-8'))
-
+            lcd.clear()
             start_time = time.time()
             elapsed_time = 0
             self.restart = False
@@ -40,7 +40,7 @@ class Worker(threading.Thread):
                 else:
                     lcd.set_color(1, 0, 0)  # Red
 
-                lcd.clear()
+                lcd.home()
                 spaces = get_spaces(self.fsym, diff_perc_24hr)
                 message = '{}{}{}%\n${}'.format(self.fsym, ' ' * spaces, '%+.2f' % (diff_perc_24hr), new)
                 lcd.message(message)
@@ -56,28 +56,6 @@ class Worker(threading.Thread):
     def restart_worker(self):
         self.restart = True
 
-def debounce(button):
-    debounce_delay = 500
-    current_button_state = True
-    last_button_state = True
-    last_debounce_time = 0
-    while True:
-        print("HERE")
-        reading = lcd.is_pressed(button)
-
-        if reading != last_button_state:
-            print("HERE0")
-            last_debounce_time = time.time()
-        if ((time.time() - last_debounce_time)/1000) > debounce_delay:
-            print("HERE1")
-            if reading != current_button_state:
-                print("HERE2")
-                current_button_state = reading
-            if current_button_state == True:
-                print("DEBOUNCED")
-                return True
-
-        last_button_state = reading
 
 if __name__ == '__main__':
     symbol_list = ['BTC', 'ETH', 'SC', 'SJCX', 'MAID']
@@ -95,27 +73,22 @@ if __name__ == '__main__':
         #     worker.restart_worker()
 
         if lcd.is_pressed(LCD.SELECT):
-            debounce(LCD.SELECT)
             worker.restart_worker()
         elif lcd.is_pressed(LCD.LEFT):
-            debounce(LCD.LEFT)
             idx -= 1
             if idx == -1:
                 idx = max - 1
             worker.set_sym(symbol_list[idx], 'USD')
             worker.restart_worker()
         elif lcd.is_pressed(LCD.RIGHT):
-            debounce(LCD.RIGHT)
             idx += 1
             if idx == max:
                 idx = 0
             worker.set_sym(symbol_list[idx], 'USD')
             worker.restart_worker()
         elif lcd.is_pressed(LCD.UP):
-            debounce(LCD.UP)
             pass
         elif lcd.is_pressed(LCD.DOWN):
-            debounce(LCD.DOWN)
             pass
 
         time.sleep(.1)
