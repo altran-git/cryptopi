95a9602747a4ca904b883463c437fba9475b4441
Andy Tran <trnandy@gmail.com>
2017-08-17 06:54:09 +0000

    Fix: Handle exceptions when url does not load

--- cryptopi.py
+++ cryptopi.py
@@ -27,47 +27,62 @@ class Worker(threading.Thread):
     def run(self):
         while True:
             url = 'https://min-api.cryptocompare.com/data/histohour?fsym={}&tsym={}&limit=24'.format(self.fsym.upper(),self.tsym.upper())
-            self.data_24hr = json.loads(urlopen(url).read().decode('utf-8'))
+            try:
+                self.data_24hr = json.loads(urlopen(url).read().decode('utf-8'))
+            except Exception as e:
+                print("URL Load error.")
+                time.sleep(.1)
+                continue
             lcd.clear()
-            if self.data_24hr['Response'].upper().strip() == 'ERROR':
-                self.break_error = False
-                message = self.data_24hr['Message'].upper()
-                for i in range((len(message) - 16)+(len(message) % 16)):
-                    if self.break_error:
-                        break
-                    lcd.clear()
-                    lcd.message('ERROR:\n{}'.format(message[i:(i + 16)]))
-                    time.sleep(0.03)
-            else:
-                start_time = time.time()
-                elapsed_time = 0
-                self.restart = False
-                while self.restart is not True and elapsed_time < 10:
-                    old = self.data_24hr['Data'][0]['close']
-                    new = self.data_24hr['Data'][-1]['close']
-
-                    diff_perc_24hr = round((new / old - 1) * 100, 2)
-
-                    if diff_perc_24hr >= 0:
-                        lcd.set_color(0, 1, 0)  # Green
-                    else:
-                        lcd.set_color(1, 0, 0)  # Red
-
-                    lcd.home()
-                    if self.toggle != True:
-                        spaces = get_spaces(self.fsym, diff_perc_24hr)
-                        if self.tsym == 'USD':
-                            message = '{}{}{}%\n${}'.format(self.fsym, ' ' * spaces, '%+.2f' % (diff_perc_24hr), new)
-                        elif self.tsym == 'BTC':
-                            message = '{}{}{}%\n{}{}'.format(self.fsym, ' ' * spaces, '%+.2f' % (diff_perc_24hr), '\x01', new)
+            try:
+                if self.data_24hr['Response'].upper().strip() == 'ERROR':
+                    self.break_error = False
+                    message = self.data_24hr['Message'].upper()
+                    for i in range((len(message) - 16)+(len(message) % 16)):
+                        if self.break_error:
+                            break
+                        lcd.clear()
+                        lcd.message('ERROR:\n{}'.format(message[i:(i + 16)]))
+                        time.sleep(0.03)
+                elif self.data_24hr['Response'].upper().strip() == 'SUCCESS':
+                    start_time = time.time()
+                    elapsed_time = 0
+                    self.restart = False
+                    while self.restart is not True and elapsed_time < 10:
+                        old = self.data_24hr['Data'][0]['close']
+                        new = self.data_24hr['Data'][-1]['close']
+                        
+                        if old == 0:
+                            diff_perc_24hr = 0
                         else:
-                            message = '{}{}{}%\n{}'.format(self.fsym, ' ' * spaces, '%+.2f' % (diff_perc_24hr), new)
-                    else:
-                        message = '\n {}'.format(new)
-                    lcd.message(message)
+                            diff_perc_24hr = round((new / old - 1) * 100, 2)
 
-                    time.sleep(.1)
-                    elapsed_time = time.time() - start_time
+                        if diff_perc_24hr >= 0:
+                            lcd.set_color(0, 1, 0)  # Green
+                        else:
+                            lcd.set_color(1, 0, 0)  # Red
+
+                        lcd.home()
+                        if self.toggle != True:
+                            spaces = get_spaces(self.fsym, diff_perc_24hr)
+                            if self.tsym == 'USD':
+                                message = '{}{}{}%\n${}'.format(self.fsym, ' ' * spaces, '%+.2f' % (diff_perc_24hr), new)
+                            elif self.tsym == 'BTC':
+                                message = '{}{}{}%\n{}{}'.format(self.fsym, ' ' * spaces, '%+.2f' % (diff_perc_24hr), '\x01', new)
+                            else:
+                                message = '{}{}{}%\n{}'.format(self.fsym, ' ' * spaces, '%+.2f' % (diff_perc_24hr), new)
+                        else:
+                            message = '\n {}'.format(new)
+                        lcd.message(message)
+
+                        time.sleep(.1)
+                        elapsed_time = time.time() - start_time
+                else:
+                    print("Error: Unknown Response")
+            except Exception as e:
+                print("Data processing error.")
+                time.sleep(.1)
+                continue
 
     def set_sym(self, fsym, tsym):
         self.fsym = fsym
@@ -172,3 +187,5 @@ if __name__ == '__main__':
             worker.set_break_error()
             worker.restart_worker()
             time.sleep(3)
+
+        time.sleep(.1)
